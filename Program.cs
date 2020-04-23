4ea7afaefad9f27b88b58c8ce26681e8bdf1533d
Kfir-G <kfirg15@gmail.com>
2020-04-23 17:49:36 +0300

    Main + Animal.cs
    Adding the main
    and the preview Animal.cs class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,72 @@
 
 namespace HW_3
 {
-    class Program
-    {
-        static void Main(string[] args)
+        enum MenuAnimal { ADD_ANIMAL, TAKE_CARE, PRINT, EXIT };
+        class Program
         {
-            Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+
+                MenuAnimal menuChoice;
+                Service service = new Service();
+                do
+                {
+                    Console.Clear();
+                    string Menu = "\nAnimal Manager \n\n\t1 - Add New Animal \n\t2 - Take care of next animal\n\t3 - Print animals List \n\t4 - Exit\n\t\t\t\t\t*\n\t\t\t\t\t *\n\t\t\t\t\t*";
+                    Console.WriteLine(Menu);
+                    menuChoice = (MenuAnimal)(int.Parse(Console.ReadLine()));
+                    if (menuChoice == MenuAnimal.EXIT)
+                    {
+                        Console.WriteLine("Have a good day!");
+                        break;
+                    }
+                    switch (menuChoice)
+                    {
+                        case MenuAnimal.ADD_ANIMAL: // Add New animal
+                            {
+                                Console.WriteLine("\n\t Add New Animal ");
+                                service.addNewAnimal();
+                                break;
+                            }
+                        case MenuAnimal.TAKE_CARE: // take care next animal
+                            {
+                                Animal ani;
+                                Console.Write("\n\tpress R for regulal animals or U for urgent animals: ");
+                                char urgent = char.Parse(Console.ReadLine());
+                                if (urgent == 'R')
+                                    ani = service.TakeCareNextAnimal(false);
+                                else
+                                    ani = service.TakeCareNextAnimal(true);
+                                if (ani != null)
+                                {
+                                    Console.WriteLine("Take care of animal:");
+                                    ani.printAnimalInfo();
+                                }
+                                else
+                                    Console.WriteLine("\n\t - There are no waiting animals in list");
+                                break;
+                            }
+                        case MenuAnimal.PRINT: // Print animals
+                            Console.WriteLine("Which animal list do you want to display?");
+                            Console.WriteLine("Regular animal list - press R\nUrgent animal list - press U");
+                            char type = char.Parse(Console.ReadLine());
+                            if (type == 'R')
+                                service.printRegularAnimals();
+                            else
+                                service.printUrgentAnimals();
+                            break;
+                        case MenuAnimal.EXIT: // Exit
+                            Console.WriteLine("Have a nice day!");
+                            break;
+                        default:
+                            Console.WriteLine("\n\t{{ Wrong Selection }}");
+                            break;
+                    }
+                    Console.WriteLine("\n\n\tPress Anykey For The Menu");
+                    Console.ReadLine();
+                } while (menuChoice != MenuAnimal.EXIT);
+
+            }
+
         }
     }
-}
